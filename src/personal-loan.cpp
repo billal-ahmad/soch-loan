d446b3442c68c1e5a06c84f55f3ad932353ecac1
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-11-25 22:12:56 +0500

    feat: Add class personal-loan

---
+++ src/personal-loan.cpp
@@ -0,0 +1,13 @@
+// src/personal-loan.cpp
+#include "../include/personal-loan.h"
+
+PersonalLoan::PersonalLoan() : _installments(0) {}
+
+PersonalLoan::PersonalLoan(std::string category, std::string amount, int installments, std::string total_payable, std::string advance)
+    : _category(category), _amount(amount), _installments(installments), _total_payable(total_payable), _advance(advance) {}
+
+std::string PersonalLoan::getCategory() const { return _category; }
+std::string PersonalLoan::getAmount() const { return _amount; }
+int PersonalLoan::getInstallments() const { return _installments; }
+std::string PersonalLoan::getTotalPayable() const { return _total_payable; }
+std::string PersonalLoan::getAdvance() const { return _advance; }
